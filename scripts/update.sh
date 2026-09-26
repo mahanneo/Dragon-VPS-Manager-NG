10a4191cc40127797c3fbc6a840dcdd45af0fe06
makia <mahanmogharrabi1380@gmail.com>
2026-09-26 10:30:34 +0330

    release: deploy makia-uat-smoke host verification command

--- scripts/update.sh
+++ scripts/update.sh
@@ -144,6 +144,7 @@ install -m 0755 "$SRC/scripts/update.sh" /usr/local/sbin/makia-update
 install -m 0755 "$SRC/scripts/backup.sh" /usr/local/sbin/makia-backup
 install -m 0755 "$SRC/scripts/uninstall.sh" /usr/local/sbin/makia-uninstall
 install -m 0755 "$SRC/scripts/doctor.sh" /usr/local/sbin/makia-doctor
+install -m 0755 "$SRC/scripts/uat-smoke.sh" /usr/local/sbin/makia-uat-smoke
 install -m 0755 "$SRC/scripts/reset-admin.sh" /usr/local/sbin/makia-reset-admin
 install -m 0755 "$SRC/upgrade.sh" /usr/local/sbin/makia-upgrade
 
